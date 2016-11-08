441a6b595a17e5eb3f31f6c21370c13f4cf5b600
AustinMahan <amahan0424@gmail.com>
2016-11-08 14:53:17 -0700

    first

---
+++ helpers.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func StationToJson(stations []Station) []string {
+	var output []string
+	for i := 0; i < len(stations); i++ {
+		jsonObj, err := json.Marshal(stations[i])
+		if err != nil {
+			fmt.Println(err)
+		}
+		output = append(output, string(jsonObj))
+	}
+	return output
+}
+
+func urlGetter(url string) []Station {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+
+	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	var jsonData []Station
+	err = json.Unmarshal([]byte(jsonDataFromHttp), &jsonData)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	return jsonData
+}
+
+func keepLines(s string, n int) string {
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+	return strings.Replace(result, "\r", "", -1)
+}
+
+type Station struct {
+	AddressInfo struct {
+		AddressLine1    string  `json:"AddressLine1"`
+		Latitude        float64 `json:"Latitude"`
+		Longitude       float64 `json:"Longitude"`
+		Postcode        string  `json:"Postcode"`
+		StateOrProvince string  `json:"StateOrProvince"`
+		Town            string  `json:"Town"`
+	} `json:"AddressInfo"`
+}
+
+func getDisanceBetween(lat1 float64, long1 float64, lat2 float64, long2 float64) float64 {
+	var dLat float64 = deg2rad(lat2 - lat1)
+	var dLon float64 = deg2rad(long2 - long1)
+	var a float64 = math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(deg2rad(lat1))*math.Cos(deg2rad(lat2))*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	var d = 3961 * c
+	return d
+}
+
+func deg2rad(deg float64) float64 {
+	return deg * (math.Pi / 180)
+}
+
+func getStationsBetween(lat1 float64, long1 float64, lat2 float64, long2 float64, stations []Station) []Station {
+	var output []Station
+
+	endDistance := getDisanceBetween(lat1, long1, lat2, long2)
+	num := len(stations)
+	for i := 0; i < num; i++ {
+		stationDist := getDisanceBetween(lat2, long2, stations[i].AddressInfo.Latitude, stations[i].AddressInfo.Longitude)
+		if endDistance > stationDist {
+			output = append(output, stations[i])
+		}
+	}
+	return output
+}
+
+func getMaxStations(num float64) string {
+	if num > 1000 {
+		return "500"
+	} else if num < 1000 && num >= 500 {
+		return "250"
+	} else {
+		return "100"
+	}
+}
+
+func toString(num float64) string {
+	return strconv.FormatFloat(num, 'f', -1, 64)
+}
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// 39.733501, -104.992597 school
+// 39.916591, -104.930168 home
+
+//39.503001, -104.755049 random
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/hello/{name}", helloName).Methods("GET")
+	router.HandleFunc("/lat/{startLat}/long/{startLong}", getLatLong).Methods("GET")
+	router.HandleFunc("/start/lat/{startLat}/long/{startLong}/end/lat/{endLat}/long/{endLong}", getBetween).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func helloName(res http.ResponseWriter, req *http.Request) {
+	name := mux.Vars(req)["name"]
+	fmt.Fprintln(res, "Hello "+name)
+}
+
+func getLatLong(res http.ResponseWriter, req *http.Request) {
+	startLat := mux.Vars(req)["startLat"]
+	startLong := mux.Vars(req)["startLong"]
+
+	url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&countrycode=US&maxresults=10&latitude=%d&longitude=%d", startLat, startLong)
+	datas := urlGetter(url)
+	output := StationToJson(datas)
+	fmt.Fprintln(res, output)
+}
+
+func getBetween(res http.ResponseWriter, req *http.Request) {
+	startLat, _ := strconv.ParseFloat(mux.Vars(req)["startLat"], 64)
+	startLong, _ := strconv.ParseFloat(mux.Vars(req)["startLong"], 64)
+	endLat, _ := strconv.ParseFloat(mux.Vars(req)["endLat"], 64)
+	endLong, _ := strconv.ParseFloat(mux.Vars(req)["endLong"], 64)
+
+	num := getDisanceBetween(startLat, startLong, endLat, endLong)
+	maxStations := getMaxStations(num)
+
+	url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&countrycode=US&latitude=%s&longitude=%s&distance=%s&&maxresults=%s", toString(startLat), toString(startLong), toString(num), maxStations)
+	datas := urlGetter(url)
+	allBetween := getStationsBetween(startLat, startLong, endLat, endLong, datas)
+	output := StationToJson(allBetween)
+	fmt.Fprint(res, output)
+}
